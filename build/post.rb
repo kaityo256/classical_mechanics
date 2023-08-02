77b1d4aca00018452af3ee4b9240d0d11973793c
H. Watanabe <kaityo@users.sourceforge.jp>
2023-08-02 19:02:40 +0900

    :memo: removes post.rb pre.rb

--- build/post.rb
+++
@@ -1,9 +0,0 @@
-while (line = gets)
-  line.gsub!('@<underscore>', '_')
-  line.gsub!('@<equal>', '=')
-  line.gsub!('@<minus>', '-')
-  line.gsub!('@<mynewline>', '\\\\\\\\')
-  line.gsub!('@<leftbracket>', '\{')
-  line.gsub!('@<rightbracket>', '\}')
-  puts line
-end
